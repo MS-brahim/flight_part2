4be3164ab8740d91c82f5400ad2fd86e2f242dcb
ms-brahim <brahim7khalil@gmail.com>
2020-06-03 03:00:58 +0100

    readme file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # flight_part2
 # Application pour la gestion des réservations des vols aériens Partie 2
 
+
+Create Database name : app_vols;
+Use app_vols;
+
+==========================
+
+cd model/data
+Insert file app_vols.sql to database (app_vols);
--- model/data/app_vols2.sql
+++ model/data/app_vols2.sql
@@ -13,35 +13,3 @@ CREATE TABLE Utilisateur (
     mot_de_passe VARCHAR(50),
     cree_a       DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE Admin (
-    id_user     INT NOT NULL PRIMARY KEY,
-    tel        VARCHAR(15) NOT NULL,
-    email      VARCHAR(254) NOT NULL,
-    mot_de_passe VARCHAR(50) NOT NULL,
-    FOREIGN KEY(id_user, tel, email, mot_de_passe) REFERENCES Utilisateur(id_user, tel, email, mot_de_passe)
-)
-
-CREATE TABLE vols (
-  id_vol int(11) NOT NULL AUTO_INCREMENT,
-  nom_vol varchar(254) DEFAULT NULL,
-  departure varchar(254) DEFAULT NULL,
-  arrival varchar(254) DEFAULT NULL,
-  d_depart datetime DEFAULT NULL,
-  d_arrival datetime DEFAULT NULL,
-  prix int(11) DEFAULT NULL,
-  place int(11) DEFAULT NULL,
-  PRIMARY KEY (id_vol)
-
-)
-
-create table Reservation
-(
-   id_reservation       int not null,
-   id_client            int not null,
-   id_vol               int not null,
-   date_reservation     int,
-   primary key (id_reservation),
-   FOREIGN KEY (id_client) REFERENCES client (id_client),
-   FOREIGN KEY (id_vol) REFERENCES Vols (id_vol)
-)
